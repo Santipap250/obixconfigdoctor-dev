711d557f8ade04281cb4d9a20f867f5abdb74815
Santipap250 <santipap350z@gmail.com>
2026-01-04 20:31:37 +0700

    Delete tests directory

--- tests/test_app_post.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def test_post_app():
-    url = "https://obixconfigdoctor.onrender.com/app"
-    r = requests.post(url, data={
-        "size": "5", "weight": "450", "battery": "4S", "style": "Freestyle",
-        "prop_size": "5", "blades": "2", "pitch": "3"
-    }, timeout=10)
-    assert r.status_code == 200
